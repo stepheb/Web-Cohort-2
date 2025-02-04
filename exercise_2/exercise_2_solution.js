ade273b028c1240d7a98011d38e1808b428fd92d
stepheb <brian.c.stephens@altria.com>
2025-02-04 20:06:52 +0000

    submitting hw

--- exercise_2/exercise_2_solution.js
+++ exercise_2/exercise_2_solution.js
@@ -1,1 +1,23 @@
 // Copy and paste js here
+function drawAceOfSpades() {
+  // Your work goes here
+  let deckId = document.getElementbyId("deck").textContent;
+  let handCount = document.getElementById("handCount").value;
+  fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${handCount}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var AceOfSpades = document.getElementById("AceOfSpades");
+      AceOfSpades.innerHTML = "";
+      data.cards.forEach((card) => {
+        var image = document.createElement("img");
+        image.src = card.image;
+        AceOfSpades.appendChild(image);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
